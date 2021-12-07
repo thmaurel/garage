7206134709734eccb24806d76409463f8619050d
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-12-07 12:27:50 +0100

    remove solution

--- src/controllers/garage_controller.js
+++ src/controllers/garage_controller.js
@@ -4,43 +4,6 @@ export default class extends Controller {
   static targets = ["carsList"]
 
   connect() {
-    this.garageName = "146"
-    this.garageUrl = `https://wagon-garage-api.herokuapp.com/${this.garageName}/cars`
-    this._refreshCars()
-  }
-
-  createCar(event) {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const myNewCar = Object.fromEntries(formData)
-    fetch(this.garageUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(myNewCar)
-    })
-    .then(() => this._refreshCars())
-  }
-
-  _refreshCars() {
-    fetch(this.garageUrl)
-    .then(response => response.json())
-    .then((data) => {
-      this.carsListTarget.innerHTML = ""
-      data.forEach(car => this._insertCarCard(car))
-    });
-  }
-
-  _insertCarCard(car) {
-    const carCard = `<div class="car">
-      <div class="car-image">
-        <img src="http://loremflickr.com/300/300/${car.brand}%20${car.model}">
-      </div>
-      <div class="car-info">
-        <h4>${car.brand} - ${car.model}</h4>
-        <p><strong>Owner:</strong> ${car.owner}</p>
-        <p><strong>Plate:</strong> ${car.plate}</p>
-      </div>
-    </div>`
-    this.carsListTarget.insertAdjacentHTML('beforeend', carCard)
+    console.log("hello from garage controller!")
   }
 }
