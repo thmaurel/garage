0933fa8035d243956b8b8ec5043fbc8a996417a0
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-12-07 11:24:24 +0100

    add hello console log

--- src/controllers/garage_controller.js
+++ src/controllers/garage_controller.js
@@ -2,4 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   // TODO: create your awesome garage!
+  connect() {
+    console.log("hello from the garage controller!")
+  }
 }
