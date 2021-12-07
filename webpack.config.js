8a1d36f224379dd6471df51bf4c44f2ea7191429
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-12-07 11:13:30 +0100

    match with webpack-boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: "./src/index.js",
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
